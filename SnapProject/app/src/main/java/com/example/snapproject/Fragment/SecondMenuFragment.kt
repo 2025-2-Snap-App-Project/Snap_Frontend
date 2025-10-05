2d4ff9bb2ffba602215dfead02c4211cf7a3b7e1
이석현 <jrkenniya@gmail.com>
2025-10-05 23:13:58 +0900

    [feat] HomeFragment 안에 넣을 메뉴 프래그먼트 파일(kt) 2개 추가

---
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/SecondMenuFragment.kt
@@ -0,0 +1,40 @@
+package com.example.snapproject.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.snapproject.databinding.FragmentSecondMenuBinding
+
+class SecondMenuFragment : Fragment() {
+    private var _binding : FragmentSecondMenuBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = SecondMenuFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
