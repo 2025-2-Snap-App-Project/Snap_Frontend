6e62e30afb61bfc129b930024930a84ee9d604ba
이석현 <jrkenniya@gmail.com>
2025-10-06 01:54:23 +0900

    [feat] 촬영하기 메뉴 클릭 이벤트 처리 - MainActivity의 리스너 사용

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/FirstMenuFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/FirstMenuFragment.kt
@@ -1,5 +1,6 @@
 package com.example.snapproject.Fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.example.snapproject.HomeViewPagerAdapter
+import com.example.snapproject.OnChildButtonClickListener
+import com.example.snapproject.R
 import com.example.snapproject.databinding.FragmentFirstMenuBinding
 
 class FirstMenuFragment : Fragment() {
     private var _binding : FragmentFirstMenuBinding? = null
     private val binding get() = _binding!!
+
+    private var listener: OnChildButtonClickListener? = null
 
     companion object {
         fun newInstance() = FirstMenuFragment()
@@ -25,6 +30,13 @@ class FirstMenuFragment : Fragment() {
         return binding.root
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnChildButtonClickListener) {
+            listener = context
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -32,6 +44,9 @@ class FirstMenuFragment : Fragment() {
     }
 
     private fun initView() = with(binding) {
+        btnCamera.setOnClickListener {
+            listener?.onChildButtonClicked(R.id.action_homeFragment_to_cameraFragment)
+        }
     }
 
     override fun onDestroy() {
