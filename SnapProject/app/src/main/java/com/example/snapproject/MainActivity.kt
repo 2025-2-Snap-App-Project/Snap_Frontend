a36088a668a31b5ed08666801baeb2299fbddd44
이석현 <jrkenniya@gmail.com>
2025-10-06 01:52:35 +0900

    [feat] MainActivity - 특정 메뉴 버튼 클릭 콜백 인터페이스 구현

--- SnapProject/app/src/main/java/com/example/snapproject/MainActivity.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/MainActivity.kt
@@ -6,12 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.snapproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+interface OnChildButtonClickListener {
+    fun onChildButtonClicked(destinationId: Int)
+}
+
+class MainActivity : AppCompatActivity(), OnChildButtonClickListener {
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var navController: NavController
+
+    override fun onChildButtonClicked(destinationId: Int) {
+        findNavController(R.id.nav_host_fragment).navigate(destinationId)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
