59a78bfffd71a2e951a2ef5cf9c1178daf6b4df6
이석현 <jrkenniya@gmail.com>
2025-10-06 00:13:14 +0900

    [feat] HomeFragment에 뷰페이저 어댑터 연결

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/HomeFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/HomeFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.example.snapproject.HomeViewPagerAdapter
 import com.example.snapproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val viewPagerAdapter by lazy { HomeViewPagerAdapter(requireActivity() as FragmentActivity) }
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +25,16 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+        viewPagerMenu.adapter = viewPagerAdapter
     }
 
     override fun onDestroy() {
