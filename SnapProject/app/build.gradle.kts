c34ff50a39955bdc2e054ab8f7767858b9d51b5a
이석현 <jrkenniya@gmail.com>
2025-10-06 14:14:51 +0900

    [feat] 뷰페이저 인디케이터 - 의존성 추가, xml에 뷰 추가

--- SnapProject/app/build.gradle.kts
+++ SnapProject/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
+    implementation(libs.dotsindicator)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
