35714a319a48e0ef6de3b192684082b8f4e9c483
이석현 <jrkenniya@gmail.com>
2025-10-07 00:19:44 +0900

    [feat] 이전 버튼, 홈 버튼 클릭 이벤트 처리 (화면 이동)

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/LoadingFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/LoadingFragment.kt
@@ -1,18 +1,12 @@
 package com.example.snapproject.Fragment
 
 import android.os.Bundle
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColorInt
 import androidx.fragment.app.Fragment
-import com.example.snapproject.HomeViewPagerAdapter
+import androidx.navigation.fragment.findNavController
 import com.example.snapproject.R
-import com.example.snapproject.databinding.FragmentHomeBinding
 import com.example.snapproject.databinding.FragmentLoadingBinding
 
 class LoadingFragment : Fragment() {
@@ -38,6 +32,13 @@ class LoadingFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+
+        binding.btnBack.setOnClickListener { // 이전 버튼 클릭 -> 촬영하기(카메라) 화면으로 이동
+            findNavController().popBackStack()
+        }
+        binding.btnHome.setOnClickListener { // "홈으로 돌아가기" 버튼 클릭 -> 홈 화면으로 이동
+            findNavController().navigate(R.id.action_loadingFragment_to_homeFragment)
+        }
     }
 
     private fun initView() = with(binding) {
