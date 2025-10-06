702824a6b6a8d92acaca059c857e69cd1ac77596
이석현 <jrkenniya@gmail.com>
2025-10-06 18:28:46 +0900

    [feat] fragment에서 context와 activity 가져오는 함수 정의

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/CameraFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/CameraFragment.kt
@@ -1,15 +1,20 @@
 package com.example.snapproject.Fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.snapproject.MainActivity
 import com.example.snapproject.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment() {
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mContext: Context
+    private lateinit var mActivity: MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +31,14 @@ class CameraFragment : Fragment() {
     ): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    // context와 activity 가져오는 함수
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mContext = context
+        mActivity = context as MainActivity
     }
 
     override fun onViewCreated(
