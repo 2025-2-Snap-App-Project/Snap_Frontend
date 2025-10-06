30b8d9de22dc1de800eec762127d1d3d80475572
이석현 <jrkenniya@gmail.com>
2025-10-06 14:54:46 +0900

    [feat] 텍스트뷰 중 "사용자" 파트만 색상 변경하기

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/HomeFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.example.snapproject.Fragment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.example.snapproject.HomeViewPagerAdapter
+import com.example.snapproject.R
 import com.example.snapproject.databinding.FragmentHomeBinding
+import androidx.core.graphics.toColorInt
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
@@ -34,6 +40,14 @@ class HomeFragment : Fragment() {
     }
 
     private fun initView() = with(binding) {
+        // 텍스트뷰에서 "사용자" 부분만 컬러 변경하기
+        val tvData: String = tvWelcome.text.toString()
+        val tvBuilder = SpannableStringBuilder(tvData)
+        val colorBlueSpan = ForegroundColorSpan(
+            "#2276FF".toColorInt())
+        tvBuilder.setSpan(colorBlueSpan, 7, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tvWelcome.text = tvBuilder
+
         viewPagerMenu.adapter = viewPagerAdapter
         viewPagerIndicator.attachTo(viewPagerMenu)
     }
