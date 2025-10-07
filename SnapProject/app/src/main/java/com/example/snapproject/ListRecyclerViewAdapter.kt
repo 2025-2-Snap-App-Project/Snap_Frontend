b0a9bf364fd8945c2d4ec1bc661fc9281ac6df73
이석현 <jrkenniya@gmail.com>
2025-10-07 17:02:06 +0900

    [feat] 소비기한 리스트 어댑터 - DiffUtil, 뷰 홀더 생성 및 바인딩

---
+++ SnapProject/app/src/main/java/com/example/snapproject/ListRecyclerViewAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.snapproject
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.snapproject.databinding.ItemListProductBinding
+import com.example.snapproject.model.ListItemData
+
+class ListRecyclerViewAdapter(
+    private val mContext: Context,
+) : RecyclerView.Adapter<ListRecyclerViewAdapter.ViewHolder>() {
+
+
+    // DiffUtil 콜백 선언 (두 개의 리스트 간 차이 계산)
+    private val differCallback =
+        object : DiffUtil.ItemCallback<ListItemData>() {
+            // ListItemData의 itemId를 통해 각각의 Item 식별
+            // itemId를 비교 -> 같으면 areContentsTheSame으로 이동
+            override fun areItemsTheSame(
+                oldItem: ListItemData,
+                newItem: ListItemData,
+            ): Boolean {
+                return oldItem.itemId == newItem.itemId
+            }
+
+            // ListItemData의 내용을 비교해서 같으면 true -> UI 변경 없음
+            // ListItemData의 내용을 비교해서 다르면 false -> UI 변경
+            override fun areContentsTheSame(
+                oldItem: ListItemData,
+                newItem: ListItemData,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+
+    // 리스트가 많으면 백그라운드에서 실행하는 게 좋음 -> AsyncListDiffer은 자동으로 백그라운드에서 실행됨
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    // ViewHolder 클래스
+    inner class ViewHolder(private val binding: ItemListProductBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(listItemData: ListItemData) {
+            // [Item의 요소 + ListItemData(data class)의 변수] 연결
+            binding.apply {
+                tvProductName.text = listItemData.productName
+                tvExpirationDate.text = listItemData.expirationDate
+            }
+        }
+    }
+
+    // 소비기한 리스트 ViewHolder 생성
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ViewHolder {
+        return ViewHolder(
+            ItemListProductBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+        )
+    }
+
+    // 소비기한 리스트 ViewHolder 바인딩
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int,
+    ) {
+        val todayData = differ.currentList[position]
+        holder.bind(todayData)
+    }
+
+    // 소비기한 리스트 Item 개수 반환
+    override fun getItemCount() = differ.currentList.size
+}
