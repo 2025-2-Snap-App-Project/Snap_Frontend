79dfa4d39bf060ac62c4e5c842a7078e0df2eebb
이석현 <jrkenniya@gmail.com>
2025-10-07 16:53:53 +0900

    [feat] 소비기한 리스트의 Item data class 파일 추가

---
+++ SnapProject/app/src/main/java/com/example/snapproject/model/ListItemData.kt
@@ -0,0 +1,8 @@
+package com.example.snapproject.model
+
+data class ListItemData(
+    val itemId: String,
+    val productName: String,
+    val expirationDate: String,
+    val isFavorite: Boolean,
+)
