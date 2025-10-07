50488313bc53377920c65a548eadcf69274b8f26
이석현 <jrkenniya@gmail.com>
2025-10-07 22:46:33 +0900

    [feat] DeleteFragment.kt - 뷰 바인딩 등 초기 세팅

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/DeleteFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/DeleteFragment.kt
@@ -5,52 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.snapproject.R
+import com.example.snapproject.databinding.FragmentDeleteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DeleteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DeleteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentDeleteBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        fun newInstance() = DeleteFragment()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delete, container, false)
+    ): View {
+        _binding = FragmentDeleteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(
-            param1: String,
-            param2: String,
-        ) = DeleteFragment().apply {
-            arguments =
-                Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initView() =
+        with(binding) {
+
         }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
