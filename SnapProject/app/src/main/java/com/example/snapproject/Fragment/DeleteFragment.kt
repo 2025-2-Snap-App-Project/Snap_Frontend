707e408e8f2a948c5726320c45f0a122e4256e65
이석현 <jrkenniya@gmail.com>
2025-10-07 22:51:38 +0900

    [feat] RecyclerView 어댑터 생성 및 연결

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/DeleteFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/DeleteFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.snapproject.ListRecyclerViewAdapter
 import com.example.snapproject.databinding.FragmentDeleteBinding
 
 class DeleteFragment : Fragment() {
     private var _binding: FragmentDeleteBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var recyclerViewAdapter: ListRecyclerViewAdapter // RecyclerView 어댑터
 
     companion object {
         fun newInstance() = DeleteFragment()
@@ -29,10 +33,16 @@ class DeleteFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        recyclerViewAdapter = ListRecyclerViewAdapter(requireContext()) // RecyclerView 어댑터 생성
+        initView()
     }
 
     private fun initView() =
         with(binding) {
+            // xml의 recyclerview와 앞서 만든 RecyclerView 어댑터 연결
+            recyclerview.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            recyclerview.adapter = recyclerViewAdapter
 
         }
 
