e8474db63e6de34521d6f75c6e49f6db76938510
이석현 <jrkenniya@gmail.com>
2025-10-07 17:57:12 +0900

    [refactor] Item 클릭 인터페이스의 매개변수 이름 변경

--- SnapProject/app/src/main/java/com/example/snapproject/ListRecyclerViewAdapter.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/ListRecyclerViewAdapter.kt
@@ -19,7 +19,7 @@ class ListRecyclerViewAdapter(
     interface OnItemClickInterface {
         fun onItemClick(
             v: View,
-            sessionId: String,
+            itemId: String,
             position: Int,
         )
     }
