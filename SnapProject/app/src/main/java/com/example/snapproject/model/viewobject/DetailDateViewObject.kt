635bf7210792cdca9839842af925ef399b4a2816
이석현 <jrkenniya@gmail.com>
2025-10-08 17:07:53 +0900

    [feat] ViewObject - open 클래스, data 클래스 각각 생성

---
+++ SnapProject/app/src/main/java/com/example/snapproject/model/viewobject/DetailDateViewObject.kt
@@ -0,0 +1,5 @@
+package com.example.snapproject.model.viewobject
+
+data class DetailDateViewObject (
+    val date: String,
+) : DetailViewObject()
---
+++ SnapProject/app/src/main/java/com/example/snapproject/model/viewobject/DetailViewObject.kt
@@ -0,0 +1,4 @@
+package com.example.snapproject.model.viewobject
+
+open class DetailViewObject {
+}
