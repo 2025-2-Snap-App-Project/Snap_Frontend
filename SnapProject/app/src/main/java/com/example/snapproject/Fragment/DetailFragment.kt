94d293cbf6404aae60b21612aeae2cee148be9c1
이석현 <jrkenniya@gmail.com>
2025-10-08 18:25:51 +0900

    [refactor] 더미 데이터 수정, 리사이클러뷰 어댑터 연결

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/DetailFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/DetailFragment.kt
@@ -12,6 +12,10 @@ import com.example.snapproject.R
 import com.example.snapproject.databinding.FragmentDetailBinding
 import com.example.snapproject.model.DetailItemData
 import com.example.snapproject.model.ListItemData
+import com.example.snapproject.model.viewobject.DetailDateViewObject
+import com.example.snapproject.model.viewobject.DetailNameViewObject
+import com.example.snapproject.model.viewobject.DetailStorageViewObject
+import com.example.snapproject.model.viewobject.DetailSummaryViewObject
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
@@ -19,14 +23,13 @@ class DetailFragment : Fragment() {
 
     private lateinit var recyclerViewAdapter: DetailRecyclerViewAdapter // RecyclerView 어댑터
 
-    private val dataName = DetailItemData(0, "제품명은 **초코파이**입니다.")
-    private val dataDate = DetailItemData(1, "소비기한은 **2025.07.22** 까지입니다.")
-    private val dataStore = DetailItemData(2, "이 제품은 **냉장고 두 번째 칸**에 있습니다.")
-    private val dataSummary =
-        DetailItemData(3, "이 제품은 닭가슴살을 주재료로 한 가공식품입니다. 전반적으로 단백질이 풍부하지만, 몇 가지 주의할 점이 있습니다. 1. **대두(콩)**과 **밀**은 대표적인 알레르기 유발 성분입니다. 해당 알레르기가 있는 분은 섭취를 피하세요. 2. **혼합제제(폴리인산나트륨, 피로인산나트륨)**는 가공식품에서 보존성과 조직감을 높이기 위한 첨가물로, 과도한 섭취 시 신장 건강에 영향을 줄 수 있습니다. 3. **L-글루타민산나트륨(MSG)**는 감칠맛을 내는 조미료로, 일반적으로 안전하지만, 일부 민감한 사람에게는 두통 등을 유발할 수 있습니다.")
+    private val dataArray: Array<DetailItemData> = arrayOf(
+        DetailItemData("DETAIL_NAME", DetailNameViewObject("초코파이")),
+        DetailItemData("DETAIL_DATE", DetailDateViewObject("2025.07.22")),
+        DetailItemData("DETAIL_STORAGE", DetailStorageViewObject("냉장고 두 번째 칸")),
+        DetailItemData("DETAIL_SUMMARY", DetailSummaryViewObject("이 제품은 닭가슴살을 주재료로 한 가공식품입니다. 전반적으로 단백질이 풍부하지만, 몇 가지 주의할 점이 있습니다. 1. **대두(콩)**과 **밀**은 대표적인 알레르기 유발 성분입니다. 해당 알레르기가 있는 분은 섭취를 피하세요. 2. **혼합제제(폴리인산나트륨, 피로인산나트륨)**는 가공식품에서 보존성과 조직감을 높이기 위한 첨가물로, 과도한 섭취 시 신장 건강에 영향을 줄 수 있습니다. 3. **L-글루타민산나트륨(MSG)**는 감칠맛을 내는 조미료로, 일반적으로 안전하지만, 일부 민감한 사람에게는 두통 등을 유발할 수 있습니다.")),
+    )
 
-    private val dataArray: ArrayList<DetailItemData> =
-        arrayListOf(dataName, dataDate, dataStore, dataSummary)
 
     companion object {
         fun newInstance() = DetailFragment()
@@ -47,7 +50,7 @@ class DetailFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-        recyclerViewAdapter = DetailRecyclerViewAdapter(requireContext()) // RecyclerView 어댑터 생성
+        recyclerViewAdapter = DetailRecyclerViewAdapter(dataArray) // RecyclerView 어댑터 생성
 
         initView()
 
@@ -64,27 +67,10 @@ class DetailFragment : Fragment() {
             // xml의 recyclerview와 앞서 만든 RecyclerView 어댑터 연결
             recyclerview.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             recyclerview.adapter = recyclerViewAdapter
-
-            addListItemData(dataArray) // 제품 상세 설명 Item에 데이터 추가 (ArrayList에 담아둔 더미 데이터)
         }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    // 리사이클러뷰 Item에 데이터 추가 -> UI 업데이트
-    private fun addListItemData(data: ArrayList<DetailItemData>) {
-        val itemList = ArrayList<DetailItemData>(data.size)
-        for (i in data) { // [입력으로 들어온 data <-> 리사이클러뷰 item data class] 매핑
-            itemList.add(
-                DetailItemData(
-                    i.itemId,
-                    i.detail
-                ),
-            )
-        }
-        // 모든 Item이 추가된 Item 리스트를 UI에 반영
-        recyclerViewAdapter.differ.submitList(itemList)
     }
 }
