e64f3d40a2c2b326519cc14b1a4808cda6d63186
이석현 <jrkenniya@gmail.com>
2025-10-08 14:42:37 +0900

    [feat] 이전 버튼, 보관하기 버튼 클릭 이벤트 처리 (화면 이동)

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/DetailFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/DetailFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.snapproject.R
 import com.example.snapproject.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -31,6 +33,13 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+
+        binding.btnBack.setOnClickListener { // 이전 버튼 클릭 -> 소비기한 리스트 화면으로 이동
+            findNavController().popBackStack()
+        }
+        binding.btnStore.setOnClickListener { // 보관하기 버튼 클릭 -> 보관하기(녹음) 화면으로 이동
+            findNavController().navigate(R.id.action_detailFragment_to_storeRecordFragment)
+        }
     }
 
     private fun initView() =
