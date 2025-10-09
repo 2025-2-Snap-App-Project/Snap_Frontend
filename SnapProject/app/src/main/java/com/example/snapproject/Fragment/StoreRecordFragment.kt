c1b8ccb695d13742248e45cc1502d3e06d1f88de
이석현 <jrkenniya@gmail.com>
2025-10-10 02:03:10 +0900

    [feat] StoreRecord 프래그먼트 kt 파일 - 뷰 바인딩 등 초기 세팅

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
@@ -5,52 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.snapproject.R
+import com.example.snapproject.databinding.FragmentStoreRecordBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StoreRecordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StoreRecordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentStoreRecordBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        fun newInstance() = StoreRecordFragment()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store_record, container, false)
+    ): View {
+        _binding = FragmentStoreRecordBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(
-            param1: String,
-            param2: String,
-        ) = StoreRecordFragment().apply {
-            arguments =
-                Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() =
+        with(binding) {
         }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
