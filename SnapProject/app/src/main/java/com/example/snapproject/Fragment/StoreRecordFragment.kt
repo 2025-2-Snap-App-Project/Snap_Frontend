7ff8e763f233f47cce774add7baee782cd926e6b
이석현 <jrkenniya@gmail.com>
2025-10-10 02:12:40 +0900

    [feat] 보관하기(녹음) 프래그먼트 - Args 가져오기, 이전 버튼 클릭 이벤트 처리

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.snapproject.databinding.FragmentStoreRecordBinding
 
 class StoreRecordFragment : Fragment() {
@@ -26,7 +28,16 @@ class StoreRecordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Safe Args로 받은 데이터 가져오기
+        val args: StoreRecordFragmentArgs by navArgs()
+        val prevPage = args.prevPage
+
         initView()
+
+        binding.btnBack.setOnClickListener { // 이전 버튼 클릭 -> 제품 상세 설명 화면으로 이동
+            findNavController().popBackStack()
+        }
     }
 
     private fun initView() =
