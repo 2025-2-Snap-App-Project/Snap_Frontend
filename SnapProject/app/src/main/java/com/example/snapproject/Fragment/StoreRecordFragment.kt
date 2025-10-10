82afe4041c12263fce93fd3010595f1671bf6399
이석현 <jrkenniya@gmail.com>
2025-10-10 23:52:14 +0900

    [feat] 보관하기 화면 - 오디오 녹음 권한 요청 로직 추가

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
@@ -1,21 +1,90 @@
 package com.example.snapproject.Fragment
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.Settings
+import android.speech.RecognizerIntent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.snapproject.Fragment.CameraFragment.Companion
+import com.example.snapproject.MainActivity
 import com.example.snapproject.databinding.FragmentStoreRecordBinding
 
 class StoreRecordFragment : Fragment() {
     private var _binding: FragmentStoreRecordBinding? = null
     private val binding get() = _binding!!
 
+    // Context, Activity 변수
+    private lateinit var mContext: Context
+    private lateinit var mActivity: MainActivity
+
     companion object {
         fun newInstance() = StoreRecordFragment()
+
+        // 필요한 권한 array 선언 및 초기화 (오디오 녹음)
+        private val PERMISSIONS_REQUIRED =
+            arrayOf(android.Manifest.permission.RECORD_AUDIO)
     }
+
+    // 앱 설정 Permission 콜백 등록 (앱 설정에서의 사용자 이벤트 처리)
+    private val settingPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (!hasPermissions(mContext)) { // 사용자가 앱 설정에서도 권한 허용을 해주지 않은 경우
+                Toast.makeText(mContext, "오디오 녹음 권한을 허용해야 앱 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+                findNavController().popBackStack() // 홈 화면 이동
+            }
+        }
+
+    // 권한 있는지 없는지 검사하는 함수
+    private fun hasPermissions(context: Context) =
+        PERMISSIONS_REQUIRED.all {
+            ContextCompat.checkSelfPermission(
+                context,
+                it,
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+    // Permission 콜백 등록 (권한 요청 다이얼로그에서의 사용자 이벤트 처리)
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val allGranted =
+                permissions.all { it.value } // allGranted : 권한 요청 다이얼로그 창에서 모두 허용했는지 (true/false)
+            if (!allGranted) { // 하나라도 거부한 경우
+                // 사용자가 다시 묻지 않음을 선택한 경우 -> shouldShowRequestPermissionRationale이 false 반환 -> noAskAgain이 true가 됨.
+                val noAskAgain =
+                    PERMISSIONS_REQUIRED.any { permission ->
+                        ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            permission,
+                        ) == PackageManager.PERMISSION_DENIED &&
+                                !ActivityCompat.shouldShowRequestPermissionRationale(
+                                    mActivity,
+                                    permission,
+                                )
+                    }
+                if (noAskAgain) { // 사용자가 다시 묻지 않음을 선택한 경우 -> 앱 설정 화면으로 이동
+                    Toast.makeText(mContext, "앱 설정에서 오디오 녹음 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+                    val intent =
+                        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            .setData("package:${mContext.packageName}".toUri())
+                    settingPermissionLauncher.launch(intent)
+                } else { // 사용자가 한 번만 거부한 경우
+                    Toast.makeText(mContext, "오디오 녹음 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+                    findNavController().popBackStack() // 제품 상세 설명 화면으로 이동
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +93,14 @@ class StoreRecordFragment : Fragment() {
     ): View {
         _binding = FragmentStoreRecordBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    // context와 activity 가져오는 함수
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mContext = context
+        mActivity = context as MainActivity
     }
 
     override fun onViewCreated(
@@ -46,8 +123,21 @@ class StoreRecordFragment : Fragment() {
         }
     }
 
+    // 시스템 설정에서 권한 허용해 준 뒤, 다시 돌아왔을 때 오디오 녹음 세팅 필요
+    override fun onResume() {
+        super.onResume()
+        if (hasPermissions(mContext)) {
+        }
+    }
+
     private fun initView() =
         with(binding) {
+            // 필요한 권한이 모두 허용된 상태가 아니라면 -> 권한 요청 Dialog 띄우기
+            if (!hasPermissions(mContext)) {
+                requestPermissionLauncher.launch(PERMISSIONS_REQUIRED)
+            } else {
+
+            }
         }
 
     override fun onDestroy() {
