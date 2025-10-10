9f13df92bcb62e00c97806160e02fdec00c73c7f
이석현 <jrkenniya@gmail.com>
2025-10-10 16:46:23 +0900

    [feat] 분석 실패 Dialog 너비 설정 (디바이스 가로 크기의 80%)

--- SnapProject/app/src/main/java/com/example/snapproject/LoadingFailureDialog.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/LoadingFailureDialog.kt
@@ -2,11 +2,17 @@ package com.example.snapproject
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.graphics.Color
+import android.graphics.Insets
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.DialogFragment
 import com.example.snapproject.databinding.DialogLoadingFailureBinding
@@ -40,8 +46,45 @@ class LoadingFailureDialog : DialogFragment() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        // 디바이스 가로 크기 계산
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val windowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val deviceWidth = windowManager.currentWindowMetricsPointCompat().x
+
+        // 디바이스 가로 크기의 80% 비율로 Dialog 너비 설정
+        params?.width = (deviceWidth * 0.8).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // 디바이스 화면 크기 구하는 함수
+    private fun WindowManager.currentWindowMetricsPointCompat(): Point {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowInsets = currentWindowMetrics.windowInsets
+            var insets: Insets = windowInsets.getInsets(WindowInsets.Type.navigationBars())
+            windowInsets.displayCutout?.run {
+                insets = Insets.max(
+                    insets,
+                    Insets.of(safeInsetLeft, safeInsetTop, safeInsetRight, safeInsetBottom)
+                )
+            }
+            val insetWidth = insets.right + insets.left
+            val insetsHeight = insets.top + insets.bottom
+            Point(
+                currentWindowMetrics.bounds.width() - insetWidth,
+                currentWindowMetrics.bounds.height() - insetsHeight
+            )
+        } else {
+            Point().apply {
+                defaultDisplay.getSize(this)
+            }
+        }
     }
 }
