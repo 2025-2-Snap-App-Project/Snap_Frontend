d46efcdec6c8ceae73a744b8f3d87aed3930f84f
이석현 <jrkenniya@gmail.com>
2025-10-10 15:52:31 +0900

    [feat] 분석 실패 Dialog를 show, Dialog 내부 버튼 클릭 이벤트 처리

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/LoadingFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/LoadingFragment.kt
@@ -1,13 +1,20 @@
 package com.example.snapproject.Fragment
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
 import com.example.snapproject.R
+import com.example.snapproject.databinding.DialogLoadingFailureBinding
 import com.example.snapproject.databinding.FragmentLoadingBinding
+import androidx.core.graphics.drawable.toDrawable
+
 
 class LoadingFragment : Fragment() {
     private var _binding: FragmentLoadingBinding? = null
@@ -32,6 +39,7 @@ class LoadingFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        showDialog() // 분석 실패 다이얼로그 show
 
         binding.btnBack.setOnClickListener { // 이전 버튼 클릭 -> 촬영하기(카메라) 화면으로 이동
             findNavController().popBackStack()
@@ -48,5 +56,24 @@ class LoadingFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    // 분석 실패 다이얼로그
+    private fun showDialog() {
+        val dialogBinding = DialogLoadingFailureBinding.inflate((LayoutInflater.from(context)))
+        val dialogBuilder = AlertDialog.Builder(requireContext()).setView(dialogBinding.root)
+
+        val showDialog = dialogBuilder.show()
+        showDialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable()) // Dialog 배경의 Radius 값 반영을 위해 추가한 코드
+        showDialog.setCanceledOnTouchOutside(false) // Dialog 바깥쪽 눌러도 취소 불가
+
+        with(dialogBinding) {
+            btnCancel.setOnClickListener { // 취소 버튼 클릭
+                showDialog.dismiss()
+            }
+            btnRetry.setOnClickListener { // 재시도 버튼 클릭
+                showDialog.dismiss()
+            }
+        }
     }
 }
