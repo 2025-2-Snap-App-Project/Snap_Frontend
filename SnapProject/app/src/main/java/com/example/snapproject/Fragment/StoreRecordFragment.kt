4bebb047d86fa8f9e5146b86c8c26650edf6c451
이석현 <jrkenniya@gmail.com>
2025-10-12 20:07:14 +0900

    [chore] 키보드 입력 관련 토스트 메시지 삭제
    Talkback으로 음성 안내해 줄 필요 없으므로

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/StoreRecordFragment.kt
@@ -150,7 +150,6 @@ class StoreRecordFragment : Fragment() {
             binding.edtTxtStore.isEnabled = true // EditText 수정 가능
             binding.edtTxtStore.setText("") // 기존에 입력해둔 내용 지우기
             binding.edtTxtStore.hint = "보관 장소를\n입력해주세요."
-            Toast.makeText(mContext, "키보드로 보관 장소를 입력해주세요.", Toast.LENGTH_SHORT).show()
             mActivity.showSoftInput(binding.edtTxtStore) // MainActivity의 키보드 보여주는 함수 호출
         }
 
