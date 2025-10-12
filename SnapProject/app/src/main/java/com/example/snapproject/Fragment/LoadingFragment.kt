4ac6e81a0fb5e927ae4f8cc59a8b6f1e9e5b92db
이석현 <jrkenniya@gmail.com>
2025-10-13 00:21:02 +0900

    [refactor] 분석 실패 다이얼로그 관련 파일 삭제 (로딩 프래그먼트에도 적용)

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/LoadingFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/LoadingFragment.kt
@@ -6,17 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.snapproject.LoadingFailureDialog
 import com.example.snapproject.MainActivity
 import com.example.snapproject.R
 import com.example.snapproject.databinding.FragmentLoadingBinding
 import com.example.snapproject.readText
 
-class LoadingFragment : Fragment(), LoadingFailureDialog.LoadingFailureDialogListener {
+class LoadingFragment : Fragment() {
     private var _binding: FragmentLoadingBinding? = null
     private val binding get() = _binding!!
 
@@ -62,11 +60,6 @@ class LoadingFragment : Fragment(), LoadingFailureDialog.LoadingFailureDialogLis
             }
         }
 
-//        // 분석 실패 다이얼로그 show
-//        val dialog = LoadingFailureDialog() // LoadingFailureDialog 인스턴스화
-//        dialog.setTargetFragment(this, 0) // targetFragment Null 에러 방지
-//        dialog.show(parentFragmentManager, "LoadingFailureDialog") // dialog 최종 show
-        
         binding.btnHome.setOnClickListener { // "홈으로 돌아가기" 버튼 클릭 -> 홈 화면으로 이동
             findNavController().navigate(R.id.action_loadingFragment_to_homeFragment)
         }
@@ -79,16 +72,5 @@ class LoadingFragment : Fragment(), LoadingFailureDialog.LoadingFailureDialogLis
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    // Dialog 내부의 "재시도" 버튼 클릭 시
-    override fun onDialogRetryClick(dialog: DialogFragment) { // dialog 사라짐
-        dialog.dismiss()
-    }
-
-    // Dialog 내부의 "취소" 버튼 클릭 시
-    override fun onDialogCancelClick(dialog: DialogFragment) { // dialog 사라짐, CameraFragment로 이동
-        dialog.dismiss()
-        findNavController().popBackStack()
     }
 }
