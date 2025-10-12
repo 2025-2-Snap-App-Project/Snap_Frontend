578d0b525d6c59cf91cf060dff05c1d4b60e5b22
이석현 <jrkenniya@gmail.com>
2025-10-12 16:27:40 +0900

    [refactor] TTSManager의 readText - 발화 완료 후 콜백 처리

--- SnapProject/app/src/main/java/com/example/snapproject/TTSManager.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/TTSManager.kt
@@ -20,8 +20,24 @@ fun initTTS(context: Context): TextToSpeech {
     return tts
 }
 
-// 입력된 String을 읽어주는 함수
-fun TextToSpeech?.readText(text: String) {
-    this?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
-    this?.playSilentUtterance(500, TextToSpeech.QUEUE_ADD, null)
+// 입력된 String을 읽어주는 함수 (입력으로 들어오는 onDone은 발화가 끝난 뒤에 실행할 콜백 함수)
+fun TextToSpeech?.readText(text: String, onDone: (() -> Unit)? = null) {
+    this?.let { tts ->
+        val utteranceId = System.currentTimeMillis().toString() // 발화 식별용 고유 ID
+
+        // 발화 진행 상태를 감지하는 리스너
+        tts.setOnUtteranceProgressListener(object : android.speech.tts.UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {}
+
+            // 발화 완료 후 입력으로 들어온 OnDone 코드 실행
+            override fun onDone(utteranceId: String?) {
+                onDone?.let { it() }
+            }
+            override fun onError(utteranceId: String?) {}
+
+        })
+
+        // 기존 발화 중단 후 입력으로 들어온 text에 대해 발화 시작
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId)
+    }
 }
