52815674f3b17ca02cd14aaa9850e54cfc4c310f
이석현 <jrkenniya@gmail.com>
2025-10-24 23:30:28 +0900

    [refactor] 응답 data class - 변수명 수정, 필요 없는 변수 삭제

--- SnapProject/app/src/main/java/com/example/snapproject/model/AnalyzeResponse.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/model/AnalyzeResponse.kt
@@ -3,7 +3,7 @@ package com.example.snapproject.model
 import com.google.gson.annotations.SerializedName
 
 data class AnalyzeResponse(
-    val `data`: Data,
+    val data: Data,
     val message: String,
     val status: Int,
     val success: Boolean,
@@ -12,8 +12,6 @@ data class AnalyzeResponse(
         @SerializedName("expiration_date")
         val expirationDate: String,
         val ingredients: String,
-        @SerializedName("item_id")
-        val itemId: String,
         @SerializedName("product_name")
         val productName: String,
         val summary: List<String>,
