d8a1d8178cc62f9845713dd28469136466ed9310
이석현 <jrkenniya@gmail.com>
2025-10-26 03:02:24 +0900

    [feat] DAO 인터페이스 - 소비기한 카테고리별 레코드 조회 메서드 정의

--- SnapProject/app/src/main/java/com/example/snapproject/model/db/ProductDao.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/model/db/ProductDao.kt
@@ -2,6 +2,7 @@ package com.example.snapproject.model.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 // Data Access Object 인터페이스
 @Dao
@@ -9,4 +10,16 @@ interface ProductDao {
     // 레코드 삽입 메서드
     @Insert
     fun insert(product: Product)
+
+    // "날짜 지남" 레코드 조회 메서드
+    @Query("SELECT * FROM ProductTable WHERE expirationDate < :today")
+    fun getListGone(today: String) : List<Product>
+
+    // "날짜 임박"(7일 이하) 레코드 조회 메서드
+    @Query("SELECT * FROM ProductTable WHERE expirationDate > :today AND expirationDate <= :sevenDaysLater")
+    fun getListImminent(today: String, sevenDaysLater: String) : List<Product>
+
+    // "날짜 여유"(7일 초과) 레코드 조회 메서드
+    @Query("SELECT * FROM ProductTable WHERE expirationDate > :sevenDaysLater")
+    fun getListPlenty(sevenDaysLater: String) : List<Product>
 }
