ffd37e4db502417a1158dd17826470302b6fc6bc
이석현 <jrkenniya@gmail.com>
2025-10-26 03:23:13 +0900

    [feat] DAO 인터페이스 - 카테고리별로 제품 레코드 개수를 리턴하는 함수 정의

--- SnapProject/app/src/main/java/com/example/snapproject/model/db/ProductDao.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/model/db/ProductDao.kt
@@ -22,4 +22,16 @@ interface ProductDao {
     // "날짜 여유"(7일 초과) 레코드 조회 메서드
     @Query("SELECT * FROM ProductTable WHERE expirationDate > :sevenDaysLater")
     fun getListPlenty(sevenDaysLater: String) : List<Product>
+
+    // "날짜 지남" 레코드 개수 리턴
+    @Query("SELECT COUNT(*) FROM ProductTable WHERE expirationDate < :today")
+    fun getCountGone(today: String) : Int
+
+    // "날짜 임박"(7일 이하) 레코드 개수 리턴
+    @Query("SELECT COUNT(*) FROM ProductTable WHERE expirationDate > :today AND expirationDate <= :sevenDaysLater")
+    fun getCountImminent(today: String, sevenDaysLater: String) : Int
+
+    // "날짜 여유"(7일 초과) 레코드 개수 리턴
+    @Query("SELECT COUNT(*) FROM ProductTable WHERE expirationDate > :sevenDaysLater")
+    fun getCountPlenty(sevenDaysLater: String) : Int
 }
