4283afc3017df0855f2029e3d8011710f28527d5
이석현 <jrkenniya@gmail.com>
2025-10-25 16:34:36 +0900

    [refactor] AnalyzeResponse 데이터 클래스 -> Serializable

--- SnapProject/app/src/main/java/com/example/snapproject/model/AnalyzeResponse.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/model/AnalyzeResponse.kt
@@ -1,13 +1,14 @@
 package com.example.snapproject.model
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class AnalyzeResponse(
     val data: Data,
     val message: String,
     val status: Int,
     val success: Boolean,
-) {
+) : Serializable {
     data class Data(
         @SerializedName("expiration_date")
         val expirationDate: String,
