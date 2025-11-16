d0798307260fe6c12fc61deba5f3c78fe8b31058
이석현 <jrkenniya@gmail.com>
2025-11-16 18:33:36 +0900

    [feat] RectView - 실제 기기 화면 크기에 맞게 좌표값 수정하는 함수 정의

--- SnapProject/app/src/main/java/com/example/snapproject/RectView.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/RectView.kt
@@ -6,4 +6,20 @@ import android.view.View
 
 // YOLO 추론 결과를 화면에 보여줄 클래스 (View 상속)
 class RectView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+    private var results: ArrayList<YoloResult>? = null
+    // 실제 기기의 화면 크기에 맞게 좌표값 수정
+    fun transformRect(results: ArrayList<YoloResult>) {
+        val scaleX = width / DataProcess.INPUT_SIZE.toFloat()
+        val scaleY = scaleX * 9f / 16f
+        val realY = width * 9f / 16f
+        val diffY = realY - height
+
+        results.forEach {
+            it.rectF.left *= scaleX
+            it.rectF.right *= scaleX
+            it.rectF.top = it.rectF.top * scaleY - (diffY / 2f)
+            it.rectF.bottom = it.rectF.bottom * scaleY - (diffY / 2f)
+        }
+        this.results = results
+    }
 }
