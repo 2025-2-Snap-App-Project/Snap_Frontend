89abd0bf7d1c167b4f6ddbad985a968d30cd5177
이석현 <jrkenniya@gmail.com>
2025-11-16 18:31:33 +0900

    [feat] RectView 클래스 파일 생성 (YOLO 추론 결과를 화면에 보여줌)

---
+++ SnapProject/app/src/main/java/com/example/snapproject/RectView.kt
@@ -0,0 +1,9 @@
+package com.example.snapproject
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+// YOLO 추론 결과를 화면에 보여줄 클래스 (View 상속)
+class RectView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+}
