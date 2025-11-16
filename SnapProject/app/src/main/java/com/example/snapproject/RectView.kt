07135326c9fdcd9700f7924e545a9fd69c5c77e7
이석현 <jrkenniya@gmail.com>
2025-11-16 18:34:03 +0900

    [feat] RectView - 라벨링된 클래스의 문자열 값 가져오는 함수 정의

--- SnapProject/app/src/main/java/com/example/snapproject/RectView.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/RectView.kt
@@ -7,6 +7,8 @@ import android.view.View
 // YOLO 추론 결과를 화면에 보여줄 클래스 (View 상속)
 class RectView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
     private var results: ArrayList<YoloResult>? = null
+    private lateinit var classes: Array<String>
+
     // 실제 기기의 화면 크기에 맞게 좌표값 수정
     fun transformRect(results: ArrayList<YoloResult>) {
         val scaleX = width / DataProcess.INPUT_SIZE.toFloat()
@@ -21,5 +23,10 @@ class RectView(context: Context, attributeSet: AttributeSet) : View(context, att
             it.rectF.bottom = it.rectF.bottom * scaleY - (diffY / 2f)
         }
         this.results = results
+    }
+
+    // 라벨링된 클래스의 문자열 값 가져오기
+    fun setClassLabel(classes: Array<String>) {
+        this.classes = classes
     }
 }
