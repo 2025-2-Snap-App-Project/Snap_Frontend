4c3578314779115038279b09b4f16e53e961e9d6
이석현 <jrkenniya@gmail.com>
2025-11-18 20:35:08 +0900

    [feat] 화면에 그려진 Rect(YOLO 추론 결과)의 크기만큼 비트맵 이미지 생성

--- SnapProject/app/src/main/java/com/example/snapproject/Fragment/CameraFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Fragment/CameraFragment.kt
@@ -6,6 +6,7 @@ import ai.onnxruntime.OrtSession
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Camera
 import android.os.Bundle
 import android.provider.Settings
@@ -309,6 +310,31 @@ class CameraFragment : Fragment() {
         val results = dataProcess.outputsToNPMSPredictions(outputs) // YOLO 추론 최종 결과를 result에 저장
         binding.rectView.transformRect(results, binding.previewCamera.width, binding.previewCamera.height) // 실제 기기 화면 크기에 맞게 좌표값 조정
         binding.rectView.invalidate() // 최종 결과를 화면에 그려줌
+
+        // 화면에 그려진 Rect 크기만큼 비트맵 이미지 생성
+        val drawRect = binding.rectView.getDrawRect() // 화면에 그려진 Rect 가져오기
+        val fullBitmap = imageProxy.toBitmap() // 전체 Preview에 대한 비트맵 이미지 생성
+
+        // drawRect를 카메라 Bitmap 크기에 맞게 변환해줄 때 필요한 변수
+        val scaleX = fullBitmap.width.toFloat() / binding.previewCamera.width
+        val scaleY = fullBitmap.height.toFloat() / binding.previewCamera.height
+
+        if (drawRect != null) { // drawRect가 화면에 표시된 상태라면
+            // drawRect에 Scale 값을 곱해서 카메라 Bitmap 크기에 맞게 변환
+            val left = (drawRect.left * scaleX).toInt()
+            val top = (drawRect.top * scaleY).toInt()
+            val width = ((drawRect.right - drawRect.left) * scaleX).toInt()
+            val height = ((drawRect.bottom - drawRect.top) * scaleY).toInt()
+
+            Log.d("croppedImg", "left: ${left}, top: ${top}, width: ${width}, height: $height")
+            Log.d("bitmapImg", "${fullBitmap.width}, ${fullBitmap.height}")
+
+            // RectView (YOLO 추론 결과 그림) 크기만큼 bitmap 이미지 생성
+            if (width > 0 && height > 0) {
+                val croppedBitmap = Bitmap.createBitmap(fullBitmap, left, top, width, height)
+                Log.d("croppedBitmap", "$croppedBitmap")
+            }
+        }
     }
 
     override fun onDestroy() {
