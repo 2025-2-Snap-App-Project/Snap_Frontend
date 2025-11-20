ca367475d0dc64335b506a4e0940d7e305143670
이석현 <jrkenniya@gmail.com>
2025-11-20 16:53:39 +0900

    [fix] TTS 발화 중첩 문제 해결

--- SnapProject/app/src/main/java/com/example/snapproject/TTSManager.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/TTSManager.kt
@@ -43,7 +43,7 @@ fun TextToSpeech?.readText(
             },
         )
 
-        // 기존 발화 중단 후 입력으로 들어온 text에 대해 발화 시작
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId)
+        // 기존 발화 완료한 뒤, 입력으로 들어온 text에 대해 발화 시작
+        tts.speak(text, TextToSpeech.QUEUE_ADD, null, utteranceId)
     }
 }
