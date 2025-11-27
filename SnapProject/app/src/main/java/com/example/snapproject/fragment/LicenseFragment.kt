cce29d0b83eb1bba4187855c692e183ee51c8563
이석현 <jrkenniya@gmail.com>
2025-11-27 16:18:24 +0900

    [feat] LicenseFragment 관련 파일 추가, nav graph 등록

---
+++ SnapProject/app/src/main/java/com/example/snapproject/fragment/LicenseFragment.kt
@@ -0,0 +1,40 @@
+package com.example.snapproject.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.snapproject.databinding.FragmentLicenseBinding
+
+
+class LicenseFragment : Fragment() {
+    private var _binding: FragmentLicenseBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentLicenseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
