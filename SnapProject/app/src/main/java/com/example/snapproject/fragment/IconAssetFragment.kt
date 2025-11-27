2ebfcd77a11a2dd443ad95b66ef0f7436e08193a
이석현 <jrkenniya@gmail.com>
2025-11-27 16:34:53 +0900

    [feat] 아이콘/에셋 라이선스 프래그먼트 관련 파일 추가

---
+++ SnapProject/app/src/main/java/com/example/snapproject/fragment/IconAssetFragment.kt
@@ -0,0 +1,37 @@
+package com.example.snapproject.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.snapproject.databinding.FragmentIconAssetBinding
+
+class IconAssetFragment : Fragment() {
+    private var _binding: FragmentIconAssetBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentIconAssetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
