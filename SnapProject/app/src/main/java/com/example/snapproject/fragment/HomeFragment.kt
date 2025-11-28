2e5571359e248c41dcfe0bb3f48ebd7a78888b65
이석현 <jrkenniya@gmail.com>
2025-11-28 23:31:44 +0900

    [chore] ktlintFormat 적용

--- SnapProject/app/src/main/java/com/example/snapproject/fragment/HomeFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/fragment/HomeFragment.kt
@@ -3,14 +3,10 @@ package com.example.snapproject.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColorInt
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.snapproject.R
