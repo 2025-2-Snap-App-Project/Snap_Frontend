0a7645ba63c17246987819f29cfdad14e657e0ed
이석현 <jrkenniya@gmail.com>
2025-12-23 11:09:21 +0900

    [feat] 제품 상세 설명 화면 - UI 변경

--- SnapProject/app/src/main/java/com/example/snapproject/fragment/DetailFragment.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/fragment/DetailFragment.kt
@@ -82,7 +82,7 @@ class DetailFragment : Fragment(), DetailIngredientsDialog.DetailIngredientsDial
         }
 
         if (prevPage == "list") { // 소비기한 리스트 화면에서 넘어온 경우
-            binding.tvStoreOrDelete.text = "제품 삭제하기" // 버튼 내부 텍스트 수정
+            binding.tvStoreOrDelete.text = "제품 삭제" // 버튼 내부 텍스트 수정
             binding.tvBack.text = "이전 화면으로 이동"
 
             // TalkBack의 contentDescription 설정
