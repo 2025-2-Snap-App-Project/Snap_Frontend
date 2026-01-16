106bb8f97ea00c2f23d5b854eb97c05f37ae839f
이석현 <jrkenniya@gmail.com>
2026-01-16 18:26:13 +0900

    [chore] 원하는 텍스트 스타일 적용하는 로직 삭제

--- SnapProject/app/src/main/java/com/example/snapproject/Utils.kt
+++ SnapProject/app/src/main/java/com/example/snapproject/Utils.kt
@@ -16,33 +16,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
 
-// TextView의 ** 사이에 텍스트 스타일(컬러, 폰트)를 적용하는 함수
-fun applyStyleBetweenAsterisks(
-    tv: TextView,
-    context: Context,
-) {
-    val tvData: String = tv.text.toString()
-    val tvBuilder = SpannableStringBuilder(tvData)
-
-    var start = tvData.indexOf("**")
-    var end = tvData.indexOf("**", start + 2)
-
-    val textFont = Typeface.create(ResourcesCompat.getFont(context, R.font.pretendard_bold), Typeface.NORMAL)
-    val textColor = "#2276FF".toColorInt()
-
-    while (start != -1 && end != -1) {
-        tvBuilder.setSpan(CustomTypefaceSpan(textFont), start + 2, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        tvBuilder.setSpan(ForegroundColorSpan(textColor), start + 2, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        tvBuilder.replace(end, end + 2, "")
-        tvBuilder.replace(start, start + 2, "")
-
-        start = tvBuilder.indexOf("**")
-        end = tvBuilder.indexOf("**", start + 2)
-    }
-    tv.text = tvBuilder
-}
-
 // LinearGradient를 적용하는 별도의 확장 메소드 정의
 fun TextView.setTextColorAsLinearGradient(colors: IntArray) {
     if (colors.isEmpty()) {
